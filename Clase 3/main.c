6a4f431a033bb5c744dc49fe6d6963367dfaf5bb
JuanmaUTN <juanmacostas99@live.com>
2017-03-27 12:14:32 -0300

    Add files via upload

--- Clase 3/main.c
+++ Clase 3/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int numeroLegajo4Cifras();
+int sueldoNeto();
+
 int main()
 {
-    printf("Hello world!\n");
+    numeroLegajo4Cifras();
     return 0;
+}
+
+int numeroLegajo4Cifras()
+{
+    int numLeg;
+    printf("Escriba el nùmero de legajo del empleado.");
+    scanf("%d", &numLeg);
+    while(numLeg < 1000 || numLeg > 9999)
+    {
+        printf("Reingrese el nùmero de legajo (1000 - 9999)");
+        scanf("%d", &numLeg);
+    }
+    return numLeg;
+}
+
+int sueldoNeto()
+{
+
 }
